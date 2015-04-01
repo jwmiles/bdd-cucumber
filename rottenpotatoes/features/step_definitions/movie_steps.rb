bc3d80b84790b85a4048ffba70080209ea2c8e78
James <jwmiles@uncc.edu>
2015-04-01 16:04:06 -0300

    Testing steps

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+	Movie.create movie
   end
-  flunk "Unimplemented"
+  
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert page.body =~ /#{e1}.*#{e2}/m, "#{e1} was not before #{e2}"
+end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -22,13 +24,40 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+  rating_list.split(',').each do |rating|
+    if uncheck
+      uncheck "ratings_#{rating}"
+    else
+      check "ratings_#{rating}"
+    end
+  end
 end
+
+
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  flunk "Unimplemented"
+  movies = Movie.all
+  
+  if movies.size == 10
+    movies.each do |movie|
+      assert page.body =~ /#{movie.title}/m, "#{movie.title} did not appear"
+    end
+  else
+    false
+  end
 end
+
+
+Then /I should not see any movies/ do
+  movies = Movie.all
+  movies.each do |movie|
+    assert true unless page.body =~ /#{movie.title}/m
+  end
+end
+
+Then /^the director of "(.*?)" should be "(.*?)"$/ do |movie_title, new_director|
+  movie = Movie.find_by_title movie_title
+  movie.director.should == new_director
+end
+
